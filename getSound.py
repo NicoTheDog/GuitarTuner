d00f8f738965e6e90c9ce26179e005545d9217b7
NicoTheDog <yu010600@gmail.com>
2023-05-13 12:15:22 +0900

    2nd コミット
    ピークを検知するまで

--- getSound.py
+++ getSound.py
@@ -1,7 +1,7 @@
 import pyaudio # 音声を録音するためのライブラリ   
-import numpy as np  
-import wave # waveファイルを扱うためのライブラリ
+import numpy as np
 import matplotlib.pyplot as plt # 描画用のライブラリ
+import scipy
 
 SAMP_RATE = 44100 #サンプリングレート
 FORMAT = pyaudio.paInt16
@@ -25,14 +25,33 @@ def Record_Stop(record_data,audio):
     record_data.close()
     audio.terminate()
 
-def Display_Record_Data(record_data):
-    data = record_data.read(1024)
-    audio_data = np.frombuffer(data,dtype='int16')
+def Detect_Peak(y):
+    return scipy.signal.find_peaks(y,prominence=0.1)
 
-    plt.plot(audio_data)
+def Display_Data(x,y):
+    peaks,_ = Detect_Peak(y)
+    plt.plot(x,y)
+    plt.scatter(x[peaks],y[peaks],color = 'red')
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("amplitude spectrum[V]")
+    plt.xlim(1,4096)
+    plt.grid()
     plt.draw()
-    plt.pause(1)
+    plt.pause(0.1)
     plt.cla()
+
+def Fourier_Transform(record_data):
+    data = record_data.read(1024)
+    # 一次元データに変換する
+    audio_data = np.frombuffer(data,dtype='int16')
+    # 取得したデータをフーリエ変換をする(複素配列)
+    F = np.fft.fft(audio_data)
+    # 振幅を求める
+    F = F / (SAMP_RATE / 2)
+    amp = np.abs(F)
+    # 周波数をもとめる
+    freq = np.fft.fftfreq(1024,d=(1/SAMP_RATE))
+    Display_Data(freq[:1024//2],amp[:1024//2])
 
 if __name__ == "__main__":
 
@@ -40,7 +59,7 @@ if __name__ == "__main__":
 
     while True:
         try:
-            Display_Record_Data(record_data)
+            Fourier_Transform(record_data)
         except KeyboardInterrupt:
             break
     
